c6471d06459b59201a7370c28c78f106a5aaf034
otumian <popecan1000@gmail.com>
2022-09-13 08:31:31 +0000

    Reactive todo-app with 'Made With Matt': part 1

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's task */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>{"Today's tasks"}</Text>
+
+        <View style={styles.items}>
+          {/* this is where the tasks will go */}
+          <Task text={"Hello 1"} />
+          <Task text={"Hello 2"} />
+          <Task text={"Hello 3"} />
+          <Task text={"Hello 4"} />
+          <Task text={"Hello 5"} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +25,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
   },
 });
